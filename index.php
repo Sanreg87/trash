d511428f4c46683107603b61776e241c82e02916
Sanreg87 <44828990+Sanreg87@users.noreply.github.com>
2019-01-17 17:07:36 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo exec('curl -Ivk https://212.122.5.164');
+$opts = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "Cookie: foo=bar\r\n" .
+              "User-agent: BROWSER-DESCRIPTION-HERE\r\n"
+  ),
+  'ssl' => array('verify_peer'=>false, 'verify_peer_name'=>false)
+);
+
+$context = stream_context_create($opts);
+
+// Open the file using the HTTP headers set above
+$file = file_get_contents('https://212.122.5.164/', false, $context);
+//echo exec('curl -Ivk https://212.122.5.164');
 ?>
